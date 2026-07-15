cf525db86a51a88258df7d60b9f3169d78c10da3
augus123 <47068969+augus123@users.noreply.github.com>
2026-07-15 09:31:36 -0400

    Update transaction hash in tx-reader.py

--- scripts/tx-reader.py
+++ scripts/tx-reader.py
@@ -3,7 +3,7 @@ from web3 import Web3
 # 1. Connect to an Ethereum Node
 w3 = Web3(Web3.HTTPProvider('YOUR_INFURA_OR_ALCHEMY_API_URL'))
 
-tx_hash = 'YOUR_SELECTED_TX_HASH'
+tx_hash = '0xc320223c521d61846e2353e8113c996215bcd798ccced104bdcabb40f4f6acfe'
 
 # 2. Fetch Transaction and Receipt
 tx = w3.eth.get_transaction(tx_hash)
